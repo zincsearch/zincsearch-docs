035007e7877217523ad082ac068276b75feae888
Prabhat Sharma <hi.prabhat@gmail.com>
2022-05-22 11:42:31 +0000

    updated to use devcontainer

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 It uses material for mkdocs.
 
+docker run --rm -it -v ${PWD}:/site squidfunk/mkdocs-material build 
+
--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,6 @@ mkdocs build
 # Move the files to S3 bucket for hosting
 aws s3 sync ./site s3://zincsearch-docs/ --profile=zinc-prod
 
-# invalidate cloudfront cache so that latest files can be served docs.zincsearch.io
-aws cloudfront create-invalidation --distribution-id E2UMT5J75TQQR1 --paths=/* --profile=zinc-prod
+# invalidate cloudfront cache so that latest files can be served docs.zincsearch.com
+aws cloudfront create-invalidation --distribution-id EYQSDB90PCYFD --paths=/* --profile=zinc-prod
 
-# invalidate cloudfront cache so that latest files can be served docs.zinclabs.io
-aws cloudfront create-invalidation --distribution-id E2U6GGPWOT7X5H --paths=/* --profile=zinc-prod
